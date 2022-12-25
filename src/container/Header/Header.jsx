11f10608216f752292e9110dae92b5e51cf530b4
mknyambura <kariuki.mercie@gmail.com>
2022-12-25 05:10:05 +0300

    update header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='header wrapper section-padding' id='home'>
+    <div className="wrapper-info">
+      <SubHeading 
+        title="Chase the new flavor"/>
+      <h1 className="header-h1">The Key to Fine Dining</h1>
+      <p className="p-opensans" style={{margin: '2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <button className="custom-button">Explore Menu</button>
+    </div>
+    <div className="wrapper-image">
+      <img src={images.welcome} alt="" />
+    </div>
   </div>
 );
 
